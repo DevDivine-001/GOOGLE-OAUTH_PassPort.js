4e8af49634118146c896aaee911a137a05d6b8be
Oscdivin <divineuwandu84@Gmail.com>
2024-11-10 22:01:39 +0100

    puting code in line

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+// require("dotenv").config()
+// const express = require("express")
+// const cors = require("cors")
+// const passport = require("passport")
+// const cookieSession = require("cookie-session")
 
+import express from "express"
+import cors from "cors"
+import cookieSession from "cookie-session"
+import dotenv from "dotenv"
+import passport from "passport"
+
+
+const app = express()
+
+dotenv.config()
+
+app.use(cookieSession({
+    name: "session",
+    keys: ["cyberwolve"],
+    maxAge: 24 * 60 * 60 * 100,
+}))
 
 
 
